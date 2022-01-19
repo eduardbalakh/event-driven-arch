502b906d67a53c250c02f617ed78ecde6ddd922e
eduardbalakh <eduardbalakh@gmaol.com>
2022-01-19 20:52:09 +0500

    Added kafka connection and dependency edited

--- twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/listener/TwitterToKafkaStatusListener.java
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/listener/TwitterToKafkaStatusListener.java
@@ -1,5 +1,9 @@
 package com.microservices.demo.twitter.to.kafka.service.listener;
 
+import com.microservices.demo.config.KafkaConfigData;
+import com.microservices.demo.kafka.avro.model.TwitterAvroModel;
+import com.microservices.demo.kafka.producer.config.service.KafkaProducer;
+import com.microservices.demo.twitter.to.kafka.service.transformer.TwitterStatusToAvroTransformerClass;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -11,8 +15,23 @@ public class TwitterToKafkaStatusListener extends StatusAdapter {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TwitterToKafkaStatusListener.class);
 
+    private final KafkaConfigData kafkaConfigData;
+    private final KafkaProducer<Long, TwitterAvroModel> kafkaProducer;
+    private final TwitterStatusToAvroTransformerClass twitterStatusToAvroTransformerClass;
+
+    public TwitterToKafkaStatusListener(KafkaConfigData kafkaConfigData,
+                                        KafkaProducer<Long, TwitterAvroModel> kafkaProducer,
+                                        TwitterStatusToAvroTransformerClass twitterStatusToAvroTransformerClass) {
+        this.kafkaConfigData = kafkaConfigData;
+        this.kafkaProducer = kafkaProducer;
+        this.twitterStatusToAvroTransformerClass = twitterStatusToAvroTransformerClass;
+    }
+
     @Override
     public void onStatus(Status status) {
-        LOGGER.info("Twitter status with text: {}", status.getText());
+        LOGGER.info("Twitter status text {} sending to kafka topic {}",
+                status.getText(), kafkaConfigData.getTopicName());
+        TwitterAvroModel twitterAvroModel = twitterStatusToAvroTransformerClass.getTwitterAvroModelFromStatus(status);
+        kafkaProducer.send(kafkaConfigData.getTopicName(), twitterAvroModel.getUserId(), twitterAvroModel);
     }
 }
---
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/transformer/TwitterStatusToAvroTransformerClass.java
@@ -0,0 +1,19 @@
+package com.microservices.demo.twitter.to.kafka.service.transformer;
+
+import com.microservices.demo.kafka.avro.model.TwitterAvroModel;
+import org.springframework.stereotype.Component;
+import twitter4j.Status;
+
+@Component
+public class TwitterStatusToAvroTransformerClass {
+
+    public TwitterAvroModel getTwitterAvroModelFromStatus(Status status) {
+        return TwitterAvroModel
+                .newBuilder()
+                .setId(status.getId())
+                .setText(status.getText())
+                .setUserId(status.getUser().getId())
+                .setCreatedAt(status.getCreatedAt().getTime())
+                .build();
+    }
+}
